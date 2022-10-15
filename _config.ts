9ac53225384be14a6ccd3bb70088aa9b4f18d09f
jyuch <jyuch@users.noreply.github.com>
2022-10-15 18:13:00 +0900

    Remove Netlify CMS

--- _config.ts
+++ _config.ts
@@ -5,7 +5,6 @@ import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
-import netlifyCMS from "lume/plugins/netlify_cms.ts";
 import pageFind from "lume/plugins/pagefind.ts";
 
 const site = lume({
@@ -25,7 +24,6 @@ site
     },
   }))
   .use(slugifyUrls({ alphanumeric: false }))
-  .use(resolveUrls())
-  .use(netlifyCMS({ netlifyIdentity: true }));
+  .use(resolveUrls());
 
 export default site;
