238dcb1f2cb0c9fb38033527bae79983f696fac5
jyuch <jyuch@users.noreply.github.com>
2025-07-04 19:58:57 +0900

    lume@v3.0.4

--- _config.ts
+++ _config.ts
@@ -1,6 +1,5 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
-import postcss from "lume/plugins/postcss.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
@@ -19,7 +18,6 @@ site
   .copy("img")
   .copy("favicon.ico")
   .copy("CNAME")
-  .use(postcss())
   .use(date())
   .use(codeHighlight())
   .use(basePath())
@@ -32,6 +30,7 @@ site
   .use(resolveUrls())
   .use(sass())
   .use(nunjucks())
-  .use(minifyHTML());
+  .use(minifyHTML())
+  .add([".scss"]);
 
 export default site;
